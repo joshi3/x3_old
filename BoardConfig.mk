26098567b640c660a3a70401674045426bb487f4
joshi3 <joshi.naga3@gmail.com>
2018-06-21 00:06:32 +0530

    ditch sepolicy Signed-off-by: joshi3 <joshi.naga3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/leeco/x3/sepolicy
+#BOARD_SEPOLICY_DIRS += device/leeco/x3/sepolicy
 
 # Seccomp Filter
 BOARD_SECCOMP_POLICY := $(LOCAL_PATH)/seccomp
